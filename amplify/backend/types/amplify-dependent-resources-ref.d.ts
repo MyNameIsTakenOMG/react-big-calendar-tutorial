ad709b387bd26f9873f27e9d72f4bfe0bb4c910e
Sean Fang <fangzhengonly@gmail.com>
2023-11-08 19:44:13 -0500

    add test api and lambda

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,18 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "testApi": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "function": {
+    "testMethodLambda": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
